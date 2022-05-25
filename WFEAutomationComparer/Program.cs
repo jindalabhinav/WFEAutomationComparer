351f1cec18b5cd44a0289fd54fd070590c56f878
SANJUAN\abhinav.jindal <abhinav.jindal@avalara.com>
2022-05-25 18:41:39 +0530

    Add project files.

---
+++ WFEAutomationComparer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace WFEAutomationComparer // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sideFilesPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "Selenium Automations");
+            var sideFiles = Directory.GetFiles(sideFilesPath, "DenverTest*");
+
+            var sideFilesTargets = GetSidesTargets(sideFiles);
+            var commonTargets = GetCommonTargets(@"D:\POCs\WFEAutomationComparer\WFEAutomationComparer\Selenium Automations\DenverTest.side", sideFilesTargets);
+            PrintCommonTargets(commonTargets);
+
+            UpdateOriginalSidesWithCommonalityInfo(commonTargets, sideFiles);
+        }
+
+        private static void UpdateOriginalSidesWithCommonalityInfo(HashSet<string> commonTargets, string[] sideFiles)
+        {
+            foreach (var sideFile in sideFiles)
+            {
+
+            }
+            //var data = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Selenium Automations", sideFile));
+        }
+
+        private static void PrintCommonTargets(HashSet<string> commonTargets)
+        {
+            foreach (var target in commonTargets)
+            {
+                Console.WriteLine(target);
+            }
+        }
+
+        private static HashSet<string> GetCommonTargets(string masterFile, Dictionary<string, HashSet<string>> sideFilesTargets)
+        {
+            var intersectionResult = sideFilesTargets[masterFile];
+            foreach (var sideFiletargets in sideFilesTargets)
+            {
+                intersectionResult.Intersect(sideFiletargets.Value);
+            }
+
+            return intersectionResult;
+        }
+
+        private static Dictionary<string, HashSet<string>> GetSidesTargets(string[] sideFiles)
+        {
+
+            var sideFilesTargets = new Dictionary<string, HashSet<string>>();
+            foreach (var sideFile in sideFiles)
+            {
+
+                var data = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Selenium Automations", sideFile));
+                var sideFileData = JsonSerializer.Deserialize<SideFileOriginalData>(data);
+                sideFilesTargets.Add(sideFile, sideFileData?.tests[0]?.commands?.Select(x => x.target)?.ToHashSet());
+            }
+
+            return sideFilesTargets;
+        }
+    }
+}
---
+++ WFEAutomationComparer/SideFileOriginalData.cs
@@ -0,0 +1,42 @@
+﻿namespace WFEAutomationComparer
+{
+    public class SideFileOriginalData
+    {
+        public string id { get; set; }
+        public string version { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public Test[] tests { get; set; }
+        public Suite[] suites { get; set; }
+        public string[] urls { get; set; }
+        public string[] plugins { get; set; }
+    }
+
+    public class Test
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public CommandArray[] commands { get; set; }
+    }
+
+    public class CommandArray
+    {
+        public string id { get; set; }
+        public string comment { get; set; }
+        public string command { get; set; }
+        public string target { get; set; }
+        public string[][] targets { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Suite
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool persistSession { get; set; }
+        public bool parallel { get; set; }
+        public int timeout { get; set; }
+        public string[] tests { get; set; }
+    }
+
+}
